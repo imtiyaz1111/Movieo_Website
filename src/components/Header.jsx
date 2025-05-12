f2096ef9fd1e8fd7f415275bee09b8cae2c2b56b
Md Imtiyaz Alam <118051060+imtiyaz1111@users.noreply.github.com>
2025-05-12 11:14:12 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.avif";
 import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import userIcon from "../assets/user.png";
 import { IoSearchOutline } from "react-icons/io5";
